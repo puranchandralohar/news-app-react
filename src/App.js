9106015640c115d4d337311da7c7899a4102cb7a
puranchandralohar <clpuran95@gmail.com>
2022-10-20 11:09:58 +0530

    Footer Updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Epaper } from "./Component/Epaper";
 import { About } from "./Component/About";
 import { Contact } from "./Component/Contact";
 import { ErrorPage } from "./Component/ErrorPage";
+import { Footer } from "./Common/Footer";
 import { BrowserRouter } from "react-router-dom";
 import { Route ,Routes } from "react-router-dom";
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/Contact" element={<Contact/>}/>
         <Route path="*" element={<ErrorPage/>}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
     </>
   );
--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -1,23 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import './Footer.css'
+import React, { useEffect, useState } from "react";
+import "./Footer.css";
 
 export function Footer() {
+  const [date, setDate] = useState(new Date());
 
-  const [date, setDate] = useState(new Date())
-    
-    useEffect(() => {
-        const runningTime = setInterval(()=>setDate(new Date()), 1000 )
-        return function cleanup() {
-            clearInterval(runningTime)
-        }
-    
-    });
-
+  useEffect(() => {
+    const runningTime = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(runningTime);
+    };
+  });
 
   return (
+    <>
     <footer>
-        <p>Made with <span className='heart'><i className="fa-solid fa-heart"></i></span> by Puran Chandra</p>
-        <p>{date.toLocaleString()}</p>
+      <div className="footer_row">
+        <div className="footer_left">
+          <p class="footer_links">
+            <a href="/">Home</a>
+            <a href="#">Blog</a>
+            <a href="#">Pricing</a>
+            <a href="#">About</a>
+            <a href="#">Faq</a>
+            <a href="#">Contact</a>         
+          </p>
+        </div>
+        <div className="footer_right">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-linkedin"></i></a>
+          <a href="#"><i class="fa fa-github"></i></a>
+          <a href="#"><i class="fa fa-slack"></i></a>
+        </div>
+      </div>
     </footer>
-  )
+     <div className="footer_bottom">
+     <p>
+       Made with{" "}
+       <span className="heart">
+         <i className="fa-solid fa-heart"></i>
+       </span>{" "}
+       by Puran Chandra | {date.toLocaleString()}
+     </p>
+   </div>
+   </>
+  );
 }
